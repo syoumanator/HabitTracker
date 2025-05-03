aa1e0862a185aca0efdcabaa8b8c134f22284c2c
Alik <alik220201@gmail.com>
2025-05-04 02:14:17 +0700

    install rest_framework

--- config/settings.py
+++ config/settings.py
@@ -18,8 +18,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'users',
-    # 'habit',
+    'habit',
 ]
 
 MIDDLEWARE = [
