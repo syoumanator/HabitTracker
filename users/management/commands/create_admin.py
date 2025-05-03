edf741b4ad2ea74612a1a54c96d897cccc8ca933
Alik <alik220201@gmail.com>
2025-05-03 22:21:46 +0700

    start project (install django and setting for start, create class User)

---
+++ users/management/commands/create_admin.py
@@ -0,0 +1,29 @@
+import os
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+from dotenv import load_dotenv
+load_dotenv()
+
+SU_email = os.getenv("SU_email")
+SU_first_name = os.getenv("SU_first_name")
+SU_last_name = os.getenv("SU_last_name")
+SU_password = os.getenv("SU_password")
+
+
+class Command(BaseCommand):
+    help = "Create superuser"
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+
+        user = User.objects.create(
+            email=SU_email,
+            first_name=SU_first_name,
+            last_name=SU_last_name,
+        )
+        user.set_password(SU_password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        self.stdout.write(self.style.SUCCESS(f"Superuser created: {user.email}"))
---
+++ users/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Почта", help_text="Введите адрес электронной почты")
+    phone_number = models.CharField(
+        max_length=20,
+        verbose_name="Телефон",
+        help_text="Введите номер телефона",
+        null=True,
+        blank=True,
+    )
+    city = models.CharField(
+        max_length=20,
+        verbose_name="Город",
+        help_text="Укажите город",
+        null=True,
+        blank=True,
+    )
+    avatar = models.ImageField(upload_to="users/avatars", verbose_name="Аватар", null=True, blank=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
