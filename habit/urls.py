5fb9d49885bfac4df746b1d387a822d6aaa616e8
Alik <alik220201@gmail.com>
2025-05-04 02:30:20 +0700

    add urls

---
+++ habit/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from habit.apps import HabitConfig
+from habit.views import HabitsListAPIView, HabitsCreateAPIView, HabitsUpdateAPIView, HabitsDestroyAPIView, HabitsRetrieveAPIView
+
+app_name = HabitConfig.name
+
+urlpatterns = [
+    path("habits/", HabitsListAPIView.as_view(), name="habits-list"),
+    path("habits/create/", HabitsCreateAPIView.as_view(), name="habits-create"),
+    path("habits/<int:pk>/update/", HabitsUpdateAPIView.as_view(), name="habits-update"),
+    path("habits/<int:pk>/", HabitsRetrieveAPIView.as_view(), name="habits-get"),
+    path("habits/<int:pk>/delete/", HabitsDestroyAPIView.as_view(), name="habits-delete"),
+]
